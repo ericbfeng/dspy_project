5f71568729ef7e48a631d9208725ea2c818b02b5
ericbfeng <ericfeng@stanford.edu>
2024-01-21 16:38:48 -0800

    add router

--- dspy_app/src/App.js
+++ dspy_app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Header from './components/Header/Header';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
