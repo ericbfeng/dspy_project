4208542ec8503ccbc56eb0632c1442ce7485998a
ericbfeng <ericfeng@stanford.edu>
2024-01-21 16:31:34 -0800

    add MUI

--- dspy_app/src/App.js
+++ dspy_app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
@@ -15,7 +16,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
