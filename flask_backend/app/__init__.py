722239db75264898e11bbffc648b468ea843c558
ericbfeng <ericfeng@stanford.edu>
2024-02-01 17:25:29 -0800

    adding sub module

--- flask_backend/app/__init__.py
+++ flask_backend/app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+import os
+import subprocess
 
 app = Flask(__name__)
+
+scone_dir = os.path.join(os.path.dirname(__file__), '..', 'data', 'ScoNe')
+if not os.path.exists(scone_dir):
+    print("ScoNe directory not found, cloning from GitHub...")
+    # Make sure the ../data directory exists; create it if it doesn't
+    os.makedirs(os.path.dirname(scone_dir), exist_ok=True)
+    # Clone the ScoNe repository
+    subprocess.run(['git', 'clone', 'https://github.com/selenashe/ScoNe.git', scone_dir], check=True)
+
 
 from app import routes
